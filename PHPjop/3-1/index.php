bac7a1bac6a1ce1862bf8bf7554a4cf4d8221c7d
show520 <112166306+show520@users.noreply.github.com>
2022-09-05 11:16:32 +0900

    3-1FizzBuzz問題

---
+++ PHPjop/3-1/index.php
@@ -0,0 +1,20 @@
+<?php
+for($i=1;$i<=100;$i++){
+    if($i%15==0){
+        echo "FizzBuzz!!";
+        echo '<br>';
+    }
+    elseif($i%3==0){
+        echo "Fizz!";
+        echo '<br>';
+    }
+    elseif($i%5==0){
+        echo "Buzz!";
+        echo '<br>';
+    }
+    else{
+        echo $i;
+        echo '<br>';
+    }
+}
+?>
