5b4bc7823ab162a78400139e45a4d0556331dad2
show520 <112166306+show520@users.noreply.github.com>
2022-09-16 20:17:42 +0900

    3-3check
    訂正

---
+++ PHPjop/3-3check1/result.php
@@ -0,0 +1,25 @@
+<?php
+$numbers=$_POST['numbers'];
+$str =$numbers;
+$num=substr($str,0,1);
+echo $num;
+echo date("Y/m/d")."運勢は";
+echo '<br>';
+echo "選ばれた数字は".$num;
+echo '<br>';
+if ($num==0){
+    echo "凶";
+}
+elseif($num>=1 && $num<=3){
+    echo "小吉";
+}
+elseif($num>=4 && $num<=6){
+    echo "中吉";
+}
+elseif($num>=7 && $num<=8){
+    echo "吉";
+}
+elseif($num==9){
+    echo "大吉";
+}
+?>
